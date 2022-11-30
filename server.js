8d7044cc58122a30538c9084d02a9d590adf94b7
sabarinathan <55176329+16BCS080@users.noreply.github.com>
2022-11-30 17:28:33 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,51 +2,27 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-//base call 
-const express = require('express')
-const app = express()
+// @CELL -1 -default
+const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const {MongoClient} = require("mongodb");
 
-//session and prodection
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
-
-//db
-const dbConn = require('./components/database')
-const alog = (user_info) => { 
-  dbConn.then(function(db) {
-  var dbo = db.db("loginsystem"); 
-  dbo.collection("users").insertOne(user_info, function(err, res) {
-                                                //  if (err) throw err;
-                                                  db.close();
-                                                });
-                                          }); 
-}
-
-let users = []
-dbConn.then(function(db) {
-  var dbo = db.db("loginsystem"); 
-  dbo.collection("users").find({}).toArray(function(err, result) {
-   // if (err) throw err;
-    console.log(result);
-    users = result;
-    db.close();
-  });
-});
-
-
-// initilize of passport configiguration 
+// @CELL -2 -developed
+// #initilize of passport configiguration 
 const initializePassport = require('./passport-config')
 
-initializePassport(
-  passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
-)
+const app = express();
+const port = 3000;
+const uri = "mongodb+srv://js-mongo-db:9OiQI6hIF1Pgxn1N@cluster0.vivhzz6.mongodb.net/loginsystem?retryWrites=true&w=majority";
+let db
+let users = []
 
 
+// SET and USE 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
@@ -59,48 +35,18 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
-app.get('/', checkAuthenticated, (req, res) => {
-  res.render('index.ejs', {  users : users  }) 
-})
+//#CELL-3 FUNCTION CALLS
+//db insert loged user
+const loguser_into_db = async (user_info) => {
+  db.collection("users").insertOne(user_info); 
+}
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('login.ejs')
-})
-
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}))
-
-app.get('/register', checkNotAuthenticated, (req, res) => {
-  res.render('register.ejs')
-})
-
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    let user_data = {
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    };
-    users.push(user_data);
-    //inserinto_dbb(user_data);
-    alog(user_data);
-    res.redirect('/login')
-  } catch {
-    res.redirect('/register')
-  }
-})
-
-app.delete('/logout', (req, res) => {
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
-  });
-})
+//initializePassport
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
@@ -117,7 +63,54 @@ function checkNotAuthenticated(req, res, next) {
   next()
 }
 
-app.listen(3000,
-  () => { 
-    console.log("server started")   
-    })
+async function start() { 
+  const client =new MongoClient(uri);
+  await client.connect()
+  db = client.db("loginsystem")
+  users = await db.collection("users").find().toArray();
+  console.log(users)
+  console.log("DB SERVER CONNECTED")
+  app.listen(port, () => {
+    console.log(` SERVER APP LISTENING ON PORT ${port}`);
+  } );
+}
+
+
+//#CELL-4 URL
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', {  users : users  }) 
+})
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs')
+})
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    let user_data = {
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    };
+    users.push(user_data); 
+    loguser_into_db(user_data);
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+})
+app.delete('/logout', (req, res) => {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+})
+start()
